236d85a70b531f3d6e58c46ed980a4c78c30a6f6
AniketJoshi111 <aniketjoshi2606@gmail.com>
2024-04-09 00:01:22 +0530

    update,delete and create

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,8 @@
 export const PORT = 5555;
 
-export const mongoDBURL =
-  'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
+export const mongoDBURL = "mongodb+srv://aniketjoshi2606:u7UmT12Zc8JbFwxJ@cluster0.evporwh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+//password  = u7UmT12Zc8JbFwxJ
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
